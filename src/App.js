88e4113d82a84562bb2f2836f1f547cb49eb29ca
Tung <chaimatroi@gmail.com>
2022-06-27 23:01:26 +0700

    Lab6

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import './App.css';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+
+    );
+  }
+}
+
+
+export default App;
